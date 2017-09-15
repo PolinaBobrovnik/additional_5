29905122a45ea75bb134e0cc2757e3be192fd0a8
Polina Bobrovnik <polinabobrovnik@gmail.com>
2017-09-15 18:05:49 +0300

    Задание 5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var countOpen = 0;
+  var countClose = 0;
+  var countLine = 0;
+  for (var i=0; i<str.length; i++) {
+  	if (str[i] === '(') {
+  		countOpen++;
+  		}
+  	if (str[i] === ')') {
+  		countClose++;
+  		}
+	if (str[i] === '[') {
+  		countOpen++;
+  		}
+  	if (str[i] === ']') {
+  		countClose++;
+  		}
+  		if (str[i] === '{') {
+  		countOpen++;
+  		}
+  	if (str[i] === '}') {
+  		countClose++;
+  		}
+  	if (str[i] === '|') {
+  		countLine++;
+  	}
+  }
+    	return (countOpen === countClose) || (countLine%2==0);
 }
