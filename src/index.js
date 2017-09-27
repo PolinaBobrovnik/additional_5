462f38c76c259ce9b3b4b7ec102a8ebbd34e22a7
Polina Bobrovnik <polinabobrovnik@gmail.com>
2017-09-27 23:19:53 +0300

    Brackets

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
- var answer = 0;
- var result;
-for (var i=0; i<bracketsConfig.length; i++) {
-  if (brackets(bracketsConfig[i][0], bracketsConfig[i][1])) {
-    answer++;
+  var brackets = [];
+  var newStr = str;
+  var hlp = str.length+1;
+
+ for (var i=0; i<bracketsConfig.length; i++){
+  brackets.push(bracketsConfig[i].join(''));
+ }
+
+  function deletel (type) {
+    newStr = newStr.replace(type, '');
   }
-}
 
-if (bracketsConfig.length == answer) {
-  return true;
-} else return false;
 
-  function brackets (first, last) {
-  for (var i=0; i<str.length; i++) {
-    if (str[i] === first) {
-      count = false;
-      for (var j=i; j<str.length; j++) {
-        if (str[j] === last) {
-          count = true;      
-          break;
-        } 
-      }
+  while (newStr.length<hlp) {
+    for (var j=0; j<brackets.length; j++) {
+      deletel(brackets[j]);
+    }
 
-      }
-} return count;
-}
+    hlp -= 2;
+  }
+
+return (newStr.length == 0); 
+
 }
